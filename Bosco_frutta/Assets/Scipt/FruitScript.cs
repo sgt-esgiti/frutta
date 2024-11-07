d2dc46f9ecff765d196ac6a63c8a368c88655020
sabrina-albanese <sabrina.albanese@edu.itspiemonte.it>
2024-11-07 14:19:07 +0100

    sistemazione funzionamento gioco (gioco funzionante beta)

--- Bosco_frutta/Assets/Scipt/FruitScript.cs
+++ Bosco_frutta/Assets/Scipt/FruitScript.cs
@@ -7,6 +7,7 @@ public class FruitScript : MonoBehaviour
 {
     public int posSpawn=0;
     public Fruit fruit;
+
     private Player playerInst;
     
 
@@ -16,9 +17,14 @@ public class FruitScript : MonoBehaviour
     
 
     public void AddPoints(){
-        playerInst.AddGamePoints(fruit.Points);
+        
+        
+        if(playerInst != null){
+            playerInst.AddGamePoints(fruit.Points);
+        }
+            
     
-        //funzionano bene queste 3 righe -> se non funziona è un problema della riga 17 (riprova quando singleton partità dalla stanza)
+
         gameObject.SetActive(false);
         gameObject.transform.position = new Vector3 (10000,0,10000);
         gameObject.transform.rotation = new Quaternion (-90,0,0,0);
